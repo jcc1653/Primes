1b340ae09d3dd7d801b620568cf3f54f6ffc4efd
jcc1653 <jcc1653@gmail.com>
2015-02-27 12:28:09 -0600

    First Commit

---
+++ Prime.java
@@ -0,0 +1,15 @@
+public class Prime
+{
+	// Main subroutine
+	public static void main( String[] args )
+	{
+		// Parse integer input
+		int n = new Integer( args[0] );
+		
+		// Determine the n'th prime number
+		int prime = PrimeUtils.getPrime( n );
+		
+		// Print the result
+		System.out.println( n + "th prime number: " + prime );
+	}
+}
---
+++ PrimeUtils.java
@@ -0,0 +1,42 @@
+public class PrimeUtils
+{	
+	/**
+	 * Utility to get the n'th prime number
+	 *
+	 * @param n the index of the prime number to return
+	 * @return the n'th prime number
+	 */
+	public static int getPrime( int n )
+	{
+		int primeCounter = 0;
+		int k;
+		
+		// Scan the primes
+		for( k = 2 ; primeCounter < n ; ++k )
+		{
+			if ( isPrime( k ) )
+				primeCounter++;
+		}
+		
+		// Because the loop increments k an extra time before
+		// realizing that we're done, return one less.
+		return k - 1;
+	}
+	
+	/**
+	 * Utility to determine whether some number is prime.
+	 *
+	 * @param n the number to check
+	 * @return true if <code>n</code> is prime, false if composite
+	 */
+	public static boolean isPrime( int n )
+	{		
+		// Scan for non-trivial divisors of n.
+		for( int k = 2 ; k < n ; k++ )
+		{
+			if ( ( n % k ) == 0 )		// if anything divides us, we're composite
+				return false;
+		}
+		return true;
+	}
+}
